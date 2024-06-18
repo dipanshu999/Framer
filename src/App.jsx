b8724355be1f2e8b74e4ffe53406d2b624700b4f
Dipanshu Pandey <dps.coders@gmail.com>
2024-06-18 23:41:57 +0530

    useScroll hook used

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {motion, stagger} from 'framer-motion';
+import {motion, useScroll} from 'framer-motion';
 export default function App() {
 
   const parentVariants={
@@ -17,25 +17,57 @@ export default function App() {
 
   const arr=[1,2,3,4,5,6,7,8]
 
-  return (
-    <div className=' bg-[#111] p-10'>
-      
-      <motion.div 
-        className=' pt-20  bg-slate-700 rounded-3xl p-10 flex flex-wrap gap-20 justify-center items-center'
-        // animate={{x:1000}} transition={{duration:2,delay:1, ease:2}} 
-        // drag dragConstraints={{x:0,y:0}}
-        variants={parentVariants} initial="hidden" animate="visible"  transition={{duration:2,delay:1}}
-      >
+  const {scrollYProgress}=useScroll();
+  console.log(scrollYProgress)
 
-       { 
-       arr.map((item,index)=>{
-        return (<motion.div key={index} variants={childVariants}  className='child w-44 h-44 rounded-2xl  bg-yellow-200'>
-                </motion.div>
-               )
-       })
-        
-        }
-      </motion.div>
+  return (
+    <div className=' bg-[#111] '>
+      <motion.div style={{scaleX:scrollYProgress}} className={`h-2 w-full bg-red-500 fixed origin-left `} ></motion.div>
+      
+      <div className='p-10'>
+          <motion.div 
+            className=' pt-20  bg-slate-700 rounded-3xl p-10 flex flex-wrap gap-20 justify-center items-center'
+            // animate={{x:1000}} transition={{duration:2,delay:1, ease:2}} 
+            // drag dragConstraints={{x:0,y:0}}
+            variants={parentVariants} initial="hidden" animate="visible"  transition={{duration:2,delay:1}}
+          >
+          
+           { 
+           arr.map((item,index)=>{
+            return (<motion.div key={index} variants={childVariants}  className='child w-44 h-44 rounded-2xl  bg-yellow-200'>
+                    </motion.div>
+                   )
+           })
+
+          }
+          </motion.div>
+      </div>
+
+
+      <div className='bg-yellow-300 w-full h-screen'>
+          <div className="textBox p-20 text-4xl ">
+            <p>  recusandae facilis, totam, optio, debitis nam voluptas maxime tempora. Natus vitae totam modi, hic facere blanditiis. Delectus voluptas libero sed odit dolorem quisquam quasi, perferendis saepe. Similique necessitatibus quisquam quas eius error, veniam, architecto aliquid dolor cumque nulla vitae distinctio excepturi! Delectus similique autem minima at. Nesciunt perspiciatis tenetur repudiandae obcaecati dolore ipsum adipisci veniam et sequi ducimus praesentium eius omnis non deleniti aspernatur blanditiis ut cupiditate officia ad quidem eos, illum voluptates. Provident, iure, delectus praesentium nostrum suscipit libero quidem, exercitationem hic maxime unde illo.</p>
+          </div>
+      </div>
+      
+      <div className='bg-lime-300 w-full h-screen'>
+          <div className="textBox p-20 text-4xl ">
+            <p>  recusandae facilis, totam, optio, debitis nam voluptas maxime tempora. Natus vitae totam modi, hic facere blanditiis. Delectus voluptas libero sed odit dolorem quisquam quasi, perferendis saepe. Similique necessitatibus quisquam quas eius error, veniam, architecto aliquid dolor cumque nulla vitae distinctio excepturi! Delectus similique autem minima at. Nesciunt perspiciatis tenetur repudiandae obcaecati dolore ipsum adipisci veniam et sequi ducimus praesentium eius omnis non deleniti aspernatur blanditiis ut cupiditate officia ad quidem eos, illum voluptates. Provident, iure, delectus praesentium nostrum suscipit libero quidem, exercitationem hic maxime unde illo.</p>
+          </div>
+      </div>
+
+      <div className='bg-blue-200 w-full h-screen'>
+          <div className="textBox p-20 text-4xl ">
+            <p>  recusandae facilis, totam, optio, debitis nam voluptas maxime tempora. Natus vitae totam modi, hic facere blanditiis. Delectus voluptas libero sed odit dolorem quisquam quasi, perferendis saepe. Similique necessitatibus quisquam quas eius error, veniam, architecto aliquid dolor cumque nulla vitae distinctio excepturi! Delectus similique autem minima at. Nesciunt perspiciatis tenetur repudiandae obcaecati dolore ipsum adipisci veniam et sequi ducimus praesentium eius omnis non deleniti aspernatur blanditiis ut cupiditate officia ad quidem eos, illum voluptates. Provident, iure, delectus praesentium nostrum suscipit libero quidem, exercitationem hic maxime unde illo.</p>
+          </div>
+      </div>
+
+      <div className='bg-orange-200 w-full h-screen'>
+          <div className="textBox p-20 text-4xl ">
+            <p>  recusandae facilis, totam, optio, debitis nam voluptas maxime tempora. Natus vitae totam modi, hic facere blanditiis. Delectus voluptas libero sed odit dolorem quisquam quasi, perferendis saepe. Similique necessitatibus quisquam quas eius error, veniam, architecto aliquid dolor cumque nulla vitae distinctio excepturi! Delectus similique autem minima at. Nesciunt perspiciatis tenetur repudiandae obcaecati dolore ipsum adipisci veniam et sequi ducimus praesentium eius omnis non deleniti aspernatur blanditiis ut cupiditate officia ad quidem eos, illum voluptates. Provident, iure, delectus praesentium nostrum suscipit libero quidem, exercitationem hic maxime unde illo.</p>
+          </div>
+      </div>
+
     </div>
   )
 }
